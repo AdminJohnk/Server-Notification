92cdaba4693bcba1a6f281406838b10a814d5b57
AdminJohnk <truongtantai07042002@gmail.com>
2024-06-21 00:21:24 +0700

    Remove unused files and dependencies

--- dist/utils/function.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getSelectData = void 0;
-const getSelectData = (select = []) => {
-    return Object.fromEntries(select.map(item => [item, 1]));
-};
-exports.getSelectData = getSelectData;
-//# sourceMappingURL=function.js.map
